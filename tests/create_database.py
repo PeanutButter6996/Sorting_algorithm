e5a2d77bb34d42989ff2beba2e581c1cb25437e6
PeanutButter69 <109911533+PeanutButter6996@users.noreply.github.com>
2023-03-09 20:50:33 +0700

    Add files via upload

---
+++ tests/create_database.py
@@ -0,0 +1,11 @@
+import random
+
+n=1000000
+arrays = [round(random.uniform(1, 1000000), 2) for i in range(n)]
+
+arrays.sort(reverse=True)
+
+# Write the arrays to a file
+with open("data_11.txt", "w") as f:
+    for array in arrays:
+        f.write(str(array) + " ")
